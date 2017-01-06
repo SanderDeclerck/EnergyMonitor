ab4625e4e5ad008a33d69d999cdf620fee2f4214
Sander Declerck <sander.declerck@transics.com>
2017-01-06 12:25:58 +0100

    Remove some files

--- Server/EnergyMonitor.Service.Model/MonthlyReport.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace EnergyMonitor.Service.Model
-{
-    public class MonthlyReport
-    {
-        public IEnumerable<EnergyUsage> UsageData { get; set; }
-    }
-}
